30af935cf91c7762c38a8bc195f03ae554b0e60a
Your Name <you@example.com>
2023-01-18 10:16:30 +0100

    .

--- main.go
+++ main.go
@@ -5,10 +5,9 @@ import (
 	"log"
 	"net/http"
 
-	"os"
-
 	"github.com/joho/godotenv"
 	"github.com/thisisrandom/emdedded-rest/database"
+	"github.com/thisisrandom/emdedded-rest/handlers"
 )
 
 func init() {
@@ -23,20 +22,18 @@ func main() {
 
 	fmt.Println(d.Instance.Statement.Vars...)
 
-	//readingHandler := handlers.NewReadingsHandler(d.Instance)
+	readingHandler := handlers.NewReadingsHandler(d.Instance)
 
 	s.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		switch method := r.Method; method {
 		case "POST":
-			w.Write([]byte("OK"))
+			readingHandler.POST(w, r)
 		case "GET":
 			w.Write([]byte("OK"))
 		}
 	})
 
-	url := fmt.Sprint(os.Getenv("IP_ADDR"), ":", os.Getenv("PORT"))
-
-	err := http.ListenAndServe(url, s)
+	err := http.ListenAndServe(":8080", s)
 	if err != nil {
 		panic(err)
 	}
